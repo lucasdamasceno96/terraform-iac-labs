784238dd373f9267c2f9e6b040a0c4ec47c7aad6
lucasdamasceno96 <ldp21k@outlook.com>
2026-03-21 17:43:51 -0300

    feat(06-state): demonstrate terraform import and state mv for manual resource adoption

---
+++ exercises/06-advanced-state/main.tf
@@ -0,0 +1,6 @@
+
+resource "google_storage_bucket" "corporate_assets" {
+
+  name     = "manual-bucket-ldp21k-terraform-iac-labs"
+  location = "US-CENTRAL1"
+}
---
+++ exercises/06-advanced-state/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type    = string
+  default = "ldp21k-terraform-iac-labs"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
